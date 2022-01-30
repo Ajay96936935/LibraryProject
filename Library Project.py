65fcb5ddf6b1fca35ff8f189553bbae216fd904b
Ajay <ajajay1420@gmail.com>
2022-01-30 11:34:19 +0530

    Intial commit

---
+++ Library Project.py
@@ -0,0 +1,72 @@
+class library:
+    def __init__(self, list ,name):
+        self.booklist = list
+        self.name = name
+        self.lenDict = {}
+
+    def displayBooks(self):
+        print(f"We Have Following in our library: {self.name}")
+        for book in self.booklist:
+            print(book)
+    def lendBooks(self,user,book):
+        if book not in self.lenDict.keys():
+            self.lenDict.update({book:user})
+            print(" Lender-Book database has been updated. You can take book now")
+
+        else:
+            print(f" Book is already being used by {self.lendDict[book]}")
+
+
+
+
+    def addBooks(self,book):
+        self.booklist.append(book)
+        print("Book has been added to book list ")
+
+    def returnBook(self,book):
+        self.lenDict.pop(book)
+
+if __name__ == '__main__':
+
+    harry = library(['Python','Rich Dad and Poor Dad','Harry Potter','C++ Basic'], "Code with Harry")
+    while(True):
+        print(f"Welcome to the {harry.name} library.Enter to your choice to continue")
+
+        print("1. Display Books")
+        print("2. Lend a book")
+        print("3. Add a Book")
+        print("4. Return a Book")
+        user_choice = int(input())
+
+        if user_choice==1:
+            harry.displayBooks()
+
+
+        elif user_choice ==2:
+            book = input("Enter the name you want to lend:")
+            user = input("Enter your name")
+            harry.lendBooks(user,book)
+
+        elif user_choice==3:
+            book = input("Enter the name of book you want to add")
+            harry.addBooks(book)
+
+        elif user_choice==4:
+            book = input("Enter the name of book you want to return")
+            harry.returnBook(book)
+        else:
+            print("Not valid option")
+
+        print(" Press q to quit and c to continue")
+        user_choice2 = ""
+
+        user_choice2 = input()
+        if user_choice2 == "q":
+            exit()
+
+        elif user_choice2 =="c":
+            continue
+
+
+
+
